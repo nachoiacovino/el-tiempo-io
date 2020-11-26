d4ed0a3d956bbf03d3695f35ed844fd6db6b099d
Nacho Iacovino <nacho.iacovino@gmail.com>
2020-11-26 14:21:26 +0100

    Modify firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,14 +4,14 @@ import 'firebase/firestore';
 import firebase from 'firebase/app';
 
 const config = {
-  apiKey: 'AIzaSyCigdKDD-PSrXD4Mqnk8xxkIf2qtNd3zZc',
-  authDomain: 'el-tiempo-io.firebaseapp.com',
-  databaseURL: 'https://el-tiempo-io.firebaseio.com',
-  projectId: 'el-tiempo-io',
-  storageBucket: 'el-tiempo-io.appspot.com',
-  messagingSenderId: '979135594790',
-  appId: '1:979135594790:web:a40317c9d0bc46c655c1e7',
-  measurementId: 'G-6YV04D2LDD',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
